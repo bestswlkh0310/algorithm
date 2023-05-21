e659a3c2807f3f34a8a5eb9205903ea3984236c5
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-05-21 14:08:27 +0900

    5/21

--- bj1009.py
+++ bj1009.py
@@ -2,9 +2,17 @@ num = int(input())
 
 for i in range(num):
     a, b = map(int, input().split())
-    # b %= 4
+    b = (b - 1) % 4 + 1
     sum = a
     for j in range(b - 1):
         sum = sum * a % 10
-    if sum == 0: print(10)
-    else: print(sum)
+    print((sum - 1) % 10 + 1)
+# 1 1 1 1 1
+# 2 4 8 6 2
+# 3 9 7 1 3
+# 4 6 4 6 4
+# 5 0 5 0 5
+# 6 6 6 6 6
+# 7 9 3 1 7
+# 8 4 2 6 8
+# 9 1 9 1 9
