f59d13ba4e8ac21e67e33d68b01b63582e301a28
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-06-13 11:25:34 +0900

    6/13

--- bj1212.py
+++ bj1212.py
@@ -1,1 +1,11 @@
-print(str(bin(int(str(int(input())), 8)))[2:])
+# print(str(bin(int(str(int(input())), 8)))[2:])
+
+import sys
+
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(int(sys.stdin.readline().strip()))
+arr.sort()
+for i in range(n):
+    sys.stdout.write(f"{arr[i]}\n")
