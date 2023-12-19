d842a525a2f0f69c6140735a59195ed335192ef3
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-19 23:26:05 +0900

    1422 20%

--- bj1422-2.py
+++ bj1422-2.py
@@ -1,3 +1,5 @@
+import functools
+
 k, n = map(int, input().split())
 
 lst = []
@@ -6,18 +8,17 @@ for _ in range(k):
     n1 = int(input())
     lst.append(n1)
 
-# 정렬: 왼쪽 내림차순 정렬
-# 베스트:  높은 자리수 중 가장 큰 수
-
-lst = list(reversed(sorted(lst, key=lambda x: x * 10 ** (k - len(str(x))) - len(str(x)) * 0.01)))
+lst = list(reversed(sorted(lst, key=lambda x: (x + 1) * 10 ** (k - len(str(x))) - 1 - len(str(x)) * 0.01)))
 
 newLst = sorted(lst, key=lambda x: 12 - len(str(x)))
 
 bst = newLst[0]
 
-# print('lst', lst)
-# print('bst', bst)
-
+for i in range(len(lst) - 1):
+    if int(str(lst[i]) + str(lst[i + 1])) < int(str(lst[i + 1]) + str(lst[i])):
+        # print('splited', lst[i], lst[i + 1])
+        lst[i], lst[i + 1] = lst[i + 1], lst[i]
+# print(lst)
 t = False
 
 for i in lst:
