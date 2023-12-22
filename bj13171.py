6480d0e99c4e30c1ce3c842cbbed6b491839d512
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-22 11:32:05 +0900

    13171

---
+++ bj13171.py
@@ -0,0 +1,23 @@
+import math
+
+d = 1_000_000_007
+
+a = int(input())
+x = int(input())
+
+n = math.floor(math.log(x, 2))
+
+arr = [(a, 1)]
+
+for i in range(1, n + 1):
+    arr.append(((arr[i - 1][0] ** 2) % d, i + 1))
+
+s = 1
+
+bArr = list(map(int, reversed(list(str(bin(x))[2:]))))
+
+for (idx, i) in enumerate(bArr):
+    if i == 1:
+        s = (s * (arr[idx][0]) % d) % d
+
+print(s)
