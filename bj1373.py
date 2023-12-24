f3f3e2a6fb8380d2ac773a61cc11e86a930df95b
bswlkh0310 <hhhello0507@gmail.com>
2023-12-25 00:38:29 +0900

    1373

--- bj1373.py
+++ bj1373.py
@@ -1,11 +1,1 @@
-lst = list(input())
-lst.reverse()
-s = 0
-for (idx, i) in enumerate(lst):
-    s += int(i) * 2 ** idx
-r = []
-while s != 0:
-    r.append(str(s % 8))
-    s //= 8
-r.reverse()
-print("".join(r))
+print(str(oct(int(input(), 2)))[2:])
