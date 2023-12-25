a6e012e065e8a83036d5f00b2baf46de0c0afda0
bswlkh0310 <hhhello0507@gmail.com>
2023-12-25 23:08:49 +0900

    14862

--- bj14862.py
+++ bj14862.py
@@ -3,17 +3,17 @@ sys.setrecursionlimit(10**6 + 1)
 
 n = int(input())
 
-arr = [False for _ in range(1_000_001)]
+d = [[0 for _ in range(2)] for _ in range(1_000_001)]
+d[0][0] = 0
+d[1][0] = 2
+d[2][0] = 7
+d[2][1] = 1
 
 def dp(x):
-    if x == 0: return 1
-    if x == 1: return 2
-    if x == 2: return 7
-    if arr[x] != False: return arr[x]
-    s = 3 * dp(x - 2) + 2 * dp(x - 1)
     for i in range(3, x + 1):
-        s += (2 * dp(x - i)) % 100_000_007
-    arr[x] = s
-    return arr[x] % 100_000_7
+        d[i][1] = (d[i - 1][1] + d[i - 3][0]) % 1_000_000_007
+        d[i][0] = (3 * d[i - 2][0] + 2 * d[i - 1][0] + 2 * d[i][1]) % 1_000_000_007
+    return d[x][0]
+
 
 print(dp(n))
