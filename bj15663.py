6b87be70747bd9e3c0ae0abbd613af41eab7d5ac
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-26 23:55:45 +0900

    15663

---
+++ bj15663.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+lst = [*sorted([*map(int, input().split())])]
+result = []
+
+def A(v, r):
+    global n
+    if len(r) == m:
+        result.append(' '.join([*map(str, r)]))
+        return
+    for (idx, i) in enumerate(v):
+        newV = v[:]
+        newR = r[:]
+        newR.append(newV[idx])
+        del newV[idx]
+        A(newV, newR)
+
+A(lst, [])
+
+dic = {}
+
+for i in result:
+    if i not in dic:
+        dic[i] = 1
+        print(i)
