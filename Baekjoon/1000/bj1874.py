c2d537de3d04954e2d0bd4fd43027cfd77e41a28
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-27 20:56:07 +0900

    1874

--- Baekjoon/1000/bj1874.py
+++ Baekjoon/1000/bj1874.py
@@ -1,33 +1,30 @@
 from collections import deque
 import sys
 
-n = int(sys.stdin.readline())
+input = sys.stdin.readline
 
-result = deque()
+n = int(input())
 
-for _ in range(n):
-    result.append(int(sys.stdin.readline()))
+result = deque([int(input()) for _ in range(n)])
+stack = deque()
 
-q = deque()
-for i in range(n):
-    q.append(i + 1)
-
-stack = []
-
+i = 1
 idx = 0
-
 r = ""
 
 while idx < n:
+    if i > n + 1:
+        print('NO')
+        exit(0)
     if len(stack) != 0 and stack[-1] == result[idx]:
-            q.append(stack.pop())
+            stack.pop()
             r += '-\n'
             idx += 1
     else:
-        if len(q) == 0:
-            print('NO')
-            sys.exit(0)
-        stack.append(q.popleft())
+        # if i == 1:
+        #     print('NO')
+        #     sys.exit(0)
+        stack.append(i)
+        i += 1
         r += '+\n'
-
 sys.stdout.write(r)
