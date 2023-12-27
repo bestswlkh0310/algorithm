9edff2991a926a03049ba5f849dd44097815bcc5
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-28 01:03:08 +0900

    1005

--- Baekjoon/1000/bj1005.py
+++ Baekjoon/1000/bj1005.py
@@ -2,44 +2,36 @@ from collections import deque
 import sys
 
 input = sys.stdin.readline
-
-
 # 2 ≤ N ≤ 1000
 # 1 ≤ K ≤ 100,000
 # 1 ≤ X, Y, W ≤ N
-
-
 for _ in range(int(input())):
     n, k = map(int, input().split())
     lst = [*map(int, input().split())]
     v = [0 for _ in range(n)]
 
-    dArr = []
-    visit = [False for _ in range(k)]
+    dArr = [[] for _ in range(n)]
 
     for _ in range(k):
-        y, x = map(int, input().split())
-        dArr.append((x - 1, y - 1))
+        x, y = map(int, input().split())
+        dArr[y - 1].append(x - 1)
 
     w = int(input()) - 1
     
     v[w] = lst[w]
 
-    q = deque()
-    q.append(w)
+    q = deque([w])
 
     while q:
         s = q.popleft()
-        for (idx, i) in enumerate(dArr):
-            start = i[0]
-            end = i[1]
-            if start == s:
-                if not visit[idx] and v[end] < v[start] + lst[end]:
-                    visit[idx] = True
-                    v[end] = v[start] + lst[end]
-                    q.append(end)
-                elif visit[idx]:
-                    v[start] -= lst[start]
+        for (idx, i) in enumerate(dArr[s]):
+            start = s
+            end = i
+            # print('start -',start, end)
+            # if start == s:
+            if v[end] < v[start] + lst[end]:
+                v[end] = v[start] + lst[end]
+                q.append(end)
 
     # print(v)
     print(max(v))
