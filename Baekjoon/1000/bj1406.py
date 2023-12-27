1294e3c7e5f1b28db2c16a981cd56f9f74d8074e
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-27 23:53:07 +0900

    1406

--- Baekjoon/1000/bj1406.py
+++ Baekjoon/1000/bj1406.py
@@ -1,20 +1,23 @@
 import sys
-str = list(sys.stdin.readline())
-n = int(sys.stdin.readline())
-pos = len(str)
-for i in range(n):
-    inp = sys.stdin.readline().strip()
-    if inp == "L":
-        if pos > 0:
-            pos -= 1
-    elif inp == "D":
-        if pos < len(str):
-            pos += 1
-    elif inp == "B":
-        if pos > 0:
-            del str[pos - 1]
-            pos -= 1
-    else:
-        str.insert(pos, inp.split()[1])
-        pos += 1
-print("".join(str))
+from collections import deque
+input = sys.stdin.readline
+
+s1 = list(input().strip('\n'))
+s2 = deque()
+
+n = int(input())
+for _ in range(n):
+    order = input().split()
+    if order[0] == 'L' and s1:
+        s2.appendleft(s1.pop())
+    elif order[0] == 'D' and s2:
+        s1.append(s2.popleft())
+    elif order[0] == 'B' and s1:
+        s1.pop()
+    elif order[0] == 'P':
+        s1.append(order[1])
+
+for values in s1:
+    print(values,end='')
+for values in s2:
+    print(values,end='')
