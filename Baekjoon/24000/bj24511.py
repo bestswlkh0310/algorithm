fde104df0ee18cf6018994e7bd41482ca3d6dfec
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-30 17:58:55 +0900

    24511

--- Baekjoon/24000/bj24511.py
+++ Baekjoon/24000/bj24511.py
@@ -1,18 +1,6 @@
-n = int(input())
-arr1 = list(map(int, input().split()))
-arr2 = list(map(int, input().split()))
-
-a = int(input())
-arr3 = list(map(int, input().split()))
-tmp = None
-last = None
-for i in arr3:
-    for (idx, j) in enumerate(arr2):
-        if arr1[idx] == 0:
-            if tmp == None:
-                tmp = arr2[idx]
-                arr2[idx] = i
-            else:
-                arr2[idx] = tmp
-                tmp = j
-                
+from collections import deque
+I=lambda:[*map(int,input().split())]
+_=I()
+q = deque([i[1]for i in zip(I(),I()) if i[0] == 0][::-1])
+N =I()[0]
+print(' '.join([*map(str, ([*q][:N] + I())[:N])]))
