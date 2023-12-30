3590236554bc9bfa54428ef9d661404f54978830
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-30 23:16:34 +0900

    2217

--- Baekjoon/2000/bj2217.py
+++ Baekjoon/2000/bj2217.py
@@ -1,8 +1,1 @@
-n = int(input())
-lst = [int(input()) for _ in range(n)]
-mx = -1
-for i in range(1, n + 1):
-    s = 0
-    for j in lst:
-        if j * i > mx:
-            s += j * i
+print(max([i * (idx + 1) for (idx, i) in enumerate([*sorted([int(input())for _ in range(int(input()))])][::-1])]))
