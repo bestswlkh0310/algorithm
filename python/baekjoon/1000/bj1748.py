76de9f79f72e69bb1ede6587fa7828146b5eb852
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-10 12:15:33 +0900

    1748

--- python/baekjoon/1000/bj1748.py
+++ python/baekjoon/1000/bj1748.py
@@ -1,12 +1,13 @@
-n = input()
-l = len(n)
-if l == 1:
-    print(n)
-    exit(0)
-q = n[0]
-n = n[1:]
-s = (int(n) + 1) * l
-for i in range(l):
-    s += 9 * (10 ** (i - 1)) * i
-print(int(s))
+# 1: 9 * 1 (1~9)
+# 2: 90 * 2 (10~99)
+# 3: 900 * 3 (100~999)
 
+n = int(input())
+
+i = 0
+s = 0
+while n - (9 * (10 ** i)) > 0:
+    n -= (9 * (10 ** i))
+    s += (9 * (10 ** i)) * (i + 1)
+    i += 1
+print(s + n * (i + 1))
