5b0cc28ddc2cde8c2b3dbc0a046d6736a796cbf8
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-13 22:48:51 +0900

    숏코딩 31등~~

--- 백준/Platinum/1422. 숫자의 신/숫자의 신.py
+++ 백준/Platinum/1422. 숫자의 신/숫자의 신.py
@@ -1,19 +1,5 @@
-k, n = map(int, input().split())
-lst = [int(input()) for _ in range(k)]
-# sort priority: length, value
-lst = [*sorted(lst, key=lambda x: (len(str(x)), x), reverse=True)]
-
-best = sorted(lst, key=lambda x: -len(str(x)))[0]
-for j in range(k - 1):
-    for i in range(k - 1 - j):
-        if int(str(lst[i]) + str(lst[i + 1])) < int(str(lst[i + 1]) + str(lst[i])):
-            lst[i], lst[i + 1] = lst[i + 1], lst[i]
-
-# print
-t = False
-for i in lst:
-    if i == best and not t:
-        t = True
-        print(str(i) * (n - k + 1), end='')
-    else:
-        print(i, end='')
+from functools import cmp_to_key as C
+T,S,I,D=int,str,input,sorted
+K,N=map(T,I().split())
+G=[*D([T(I()) for _ in' '*K],key=lambda x:(len(S(x)),x))]
+print(''.join([*map(S,D(G+[G[-1]]*(N-K),key=C(lambda O,P:T(S(P)+S(O))-T(S(O)+S(P)))))]))
