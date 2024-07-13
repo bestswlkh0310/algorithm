4693c781e962df968ac5597e2d27ea38f5c11248
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-13 23:11:06 +0900

    숫자의신~

--- 백준/Platinum/1422. 숫자의 신/숫자의 신.py
+++ 백준/Platinum/1422. 숫자의 신/숫자의 신.py
@@ -1,5 +1,2 @@
-from functools import cmp_to_key as C
-T,S,I,D=int,str,input,sorted
-K,N=map(T,I().split())
-G=[*D([T(I()) for _ in' '*K],key=lambda x:(len(S(x)),x))]
-print(''.join([*map(S,D(G+[G[-1]]*(N-K),key=C(lambda O,P:T(S(P)+S(O))-T(S(O)+S(P)))))]))
+K,N,*G=map(int,open(0).read().split())
+print(*sorted(G+[max(G)]*(N-K),key=lambda O:str(O)*9)[::-1],sep='')
